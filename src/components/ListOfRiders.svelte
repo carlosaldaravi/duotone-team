2d1d10efe216d10f31b7ed5270c3b31df1715c02
Carlos Aldaravi <carlosaldaravi11@gmail.com>
2023-07-10 20:48:39 +0100

    First commit

---
+++ src/components/ListOfRiders.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Rider from './Rider.svelte'
+	import { riders } from '../data/riders'
+
+	// const params = Astro.params
+	// console.log({ params })
+
+	// const search = Astro.url.search
+	// console.log({search});
+
+	// const disciplineSelected = Astro.url.searchParams.get('discipline')! || ''
+	// let filteredRiders = []
+	// disciplineSelected !== ''
+	// 	? (filteredRiders = riders.filter((rider) => rider.discipline.includes(disciplineSelected)))
+	// 	: (filteredRiders = riders)
+</script>
+
+<ul class="grid grid-cols-2 md:grid-cols-3 gap-3 px-4">
+	<!-- {
+		filteredRiders.map(({ id, name, img }) => {
+			return (
+				<li>
+					<Rider id={id} name={name} img={img} />
+				</li>
+			)
+		})
+	} -->
+	{#each riders as { id, name, img }}
+		<li>
+			<Rider {id} {name} {img} />
+		</li>
+	{/each}
+</ul>
---
+++ src/data/riders.js
@@ -0,0 +1,248 @@
+export const riders = [
+	{
+		id: 'airton',
+		name: 'Airton Cozzolino',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Airton_Cozzolino.jpg',
+		discipline: ['wave']
+	},
+	{
+		id: 'liam',
+		name: 'Liam Whaley',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Liam_Profile_Pic.jpg',
+		discipline: ['bigair', 'freestyle']
+	},
+	{
+		id: 'principi',
+		name: 'Andrea Principi',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Andrea_Principi_Duotone_Kiteboarding_Young_Blood.png',
+		discipline: ['youngblood']
+	},
+	{
+		id: 'mikaili',
+		name: 'Mikaili Sol',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Mikaili_Sol.jpg',
+		discipline: ['wave', 'bigair', 'freestyle']
+	},
+	{
+		id: 'matchu',
+		name: 'Matchu Lopes',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Matchu_Lopes.jpg',
+		discipline: ['wave']
+	},
+	{
+		id: 'coccoluto',
+		name: 'Gianmaria Coccoluto',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Gianmaria_Coccoluto.jpg',
+		discipline: ['freestyle']
+	},
+	{
+		id: 'carew',
+		name: 'James Carew',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/James_Carew.jpg',
+		discipline: ['wave', 'bigair']
+	},
+	{
+		id: 'walker',
+		name: 'Lasse Walker',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Lasse_Walker.jpg',
+		discipline: ['bigair']
+	},
+	{
+		id: 'hadlow',
+		name: 'Aaron Hadlow',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Aaron_Hadlow.jpg',
+		discipline: ['wakestyle', 'bigair', 'freestyle']
+	},
+	{
+		id: 'romeu',
+		name: 'Reno Romeu',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Reno_Romeo.jpg',
+		discipline: ['bigair', 'freestyle']
+	},
+	{
+		id: 'vaniersel',
+		name: 'Pippa Van Iersel',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/pippa_van_irsel.jpg',
+		discipline: ['bigair', 'freestyle']
+	},
+	{
+		id: 'spiessberger',
+		name: 'Stefan Spiessberger',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Stefan_Spiessberger.jpg',
+		discipline: ['freestyle']
+	},
+	{
+		id: 'matos',
+		name: 'Pedro Matos',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Pedor_Matos_team_page_profile_pic.png',
+		discipline: ['wave']
+	},
+	{
+		id: 'font',
+		name: 'Noè Font',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Noe_Font.jpg',
+		discipline: ['wakestyle']
+	},
+	{
+		id: 'novotna',
+		name: 'Paula Novotná',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Paula_Novotna.jpg',
+		discipline: ['freestyle']
+	},
+	{
+		id: 'arnaus',
+		name: 'Rita Arnaus',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Rita_Arnaus.png',
+		discipline: ['freestyle']
+	},
+	{
+		id: 'garat',
+		name: 'Valentin Garat',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Val.JPG',
+		discipline: ['freestyle']
+	},
+	{
+		id: 'castro',
+		name: 'Julia Castro',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Julia_Castro.jpg',
+		discipline: ['freestyle']
+	},
+	{
+		id: 'cunningham',
+		name: 'Craig Cunningham',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Craig_Cunningham.jpg',
+		discipline: ['wakestyle', 'freestyle']
+	},
+	{
+		id: 'crathern',
+		name: 'Lewis Crathern',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Lewis_Crathern.jpg',
+		discipline: ['bigair']
+	},
+	{
+		id: 'tronet',
+		name: 'Jeremie Tronet',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Jeremie_Tronet.jpg',
+		discipline: ['bigair', 'freestyle']
+	},
+	{
+		id: 'carroll',
+		name: 'Colleen Carroll',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Colleen_Carroll.jpg',
+		discipline: ['wakestyle', 'freestyle']
+	},
+	{
+		id: 'cloetens',
+		name: 'Jeremoe Cloetens',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Jerome_Cloetens.jpg',
+		discipline: ['freestyle']
+	},
+	{
+		id: 'leon',
+		name: 'Claudia León',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Claudia_Leon_Martinez.jpg',
+		discipline: ['freestyle']
+	},
+	{
+		id: 'lambrecht',
+		name: 'Nathalie Lambrecht',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Duotone_Kiteboarding_Nathalie_Lambrecht_rider_profile.jpg',
+		discipline: ['bigair', 'freestyle']
+	},
+	{
+		id: 'hebert',
+		name: 'Tom Hebert',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Tom_Hebert.jpg',
+		discipline: ['bigair']
+	},
+	{
+		id: 'bagnoli',
+		name: 'Francesca Bagnoli',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Francesca_Bagnoli.jpg',
+		discipline: ['freestyle']
+	},
+	{
+		id: 'ribeiro',
+		name: 'Sebastian Ribeiro',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Sebastian_Ribero.jpg',
+		discipline: ['wave']
+	},
+	{
+		id: 'court',
+		name: 'Tom Court',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Tom_Court_Streubel_022.jpg',
+		discipline: ['wakestyle', 'freestyle']
+	},
+	{
+		id: 'steindl',
+		name: 'Gabi Steindl',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Gabi_Steindl.jpg',
+		discipline: ['wave']
+	},
+	{
+		id: 'riveras',
+		name: 'Jan Marcos Riveras',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Jan_Marcos_Riveiras.jpg',
+		discipline: ['wave']
+	},
+	{
+		id: 'garashchenko',
+		name: 'Artem Garashchenko',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Artem_Garashenko.jpg',
+		discipline: ['wakestyle', 'freestyle']
+	},
+	{
+		id: 'jungo',
+		name: 'Manuela Jungo',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Manuela_Jungo.jpg',
+		discipline: ['freestyle']
+	},
+	{
+		id: 'mclaughlin',
+		name: 'Patri Mclaughlin',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Patri_McLaughlin.jpg',
+		discipline: ['wave']
+	},
+	{
+		id: 'ulrich',
+		name: 'Edgar Ulrich',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Edgar_Ulrich_Duotone_Kiteboarding_Young_Blood.png',
+		discipline: ['youngblood']
+	},
+	{
+		id: 'losserand',
+		name: 'Camille Losserand',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Camille_Losserand_Duotone_Kiteboarding_Young_Blood.png',
+		discipline: ['youngblood']
+	},
+	{
+		id: 'merten',
+		name: 'Keanu Merten',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Keanu_Merten_Duotone_Kiteboarding_Young_Blood.png',
+		discipline: ['youngblood']
+	},
+	{
+		id: 'gomez',
+		name: 'Beto Gomez',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Betico_Gomez_Duotone_Kiteboarding_Young_Blood.png',
+		discipline: ['youngblood']
+	},
+	{
+		id: 'tullett',
+		name: 'Max Tullett',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Max_Tullett_Team_pic_Duotone_Kiteboarding_Young_Blood.png',
+		discipline: ['youngblood']
+	},
+	{
+		id: 'dabi-ribeiro',
+		name: 'Davi Ribeiro',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Davi_Ribeiro_Da_Silva_Duotone_Kiteboarding_Young_Blood.png',
+		discipline: ['youngblood']
+	},
+	{
+		id: 'costa',
+		name: 'Guilherme Costa',
+		img: 'https://www.duotonesports.com/fileadmin/user_upload/Guilhermo_Costa_Duotone_Kiteboarding_Young_Blood.png',
+		discipline: ['youngblood']
+	}
+]
