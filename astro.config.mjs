0944467a61153ac4e1bcd38d5a0da3c7676c09ef
Carlos Aldaravi <carlosaldaravi11@gmail.com>
2023-07-10 22:57:52 +0100

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import svelte from '@astrojs/svelte'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
+import vercel from '@astrojs/vercel/serverless';
 
-import vercel from '@astrojs/vercel/serverless'
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), svelte()],
-	output: 'server',
-	adapter: vercel()
-})
+  integrations: [tailwind(), svelte()],
+  output: 'server',
+  adapter: netlify()
+});
