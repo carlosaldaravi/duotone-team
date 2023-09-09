b7df57a65f20b31a5014a9c6a3594ae20c63e66d
Carlos Aldaravi <carlosaldaravi11@gmail.com>
2023-09-09 17:13:25 +0200

    update riders data

--- src/data/riders.js
+++ src/data/riders.js
@@ -2,247 +2,187 @@ export const riders = [
 	{
 		id: 'airton',
 		name: 'Airton Cozzolino',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Airton_Cozzolino.jpg',
-		discipline: ['wave']
+		img: 'https://a.storyblok.com/f/120582/1257x1257/709d7f1722/athlete_test.png/m/716x716/filters:grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['wave', 'bigair']
 	},
 	{
 		id: 'liam',
 		name: 'Liam Whaley',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Liam_Profile_Pic.jpg',
+		img: 'https://a.storyblok.com/f/120582/4000x2667/4550a17e54/ck_221130_duotone_0306.jpg/m/716x716/filters:grayscale(false):blur(0):quality(80):format(webp)',
 		discipline: ['bigair', 'freestyle']
 	},
 	{
 		id: 'principi',
 		name: 'Andrea Principi',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Andrea_Principi_Duotone_Kiteboarding_Young_Blood.png',
-		discipline: ['youngblood']
+		img: 'https://a.storyblok.com/f/120582/2055x1958/3327791d2f/duotone_kiteboarding_andreaprincip_tarifa_profilepicture.jpg/m/716x716/filters:grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['bigair']
 	},
 	{
 		id: 'mikaili',
 		name: 'Mikaili Sol',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Mikaili_Sol.jpg',
-		discipline: ['wave', 'bigair', 'freestyle']
+		img: 'https://a.storyblok.com/f/120582/2831x2342/a886ba3866/duotone_kiteboarding_mikailisol_capetown_profileshots.jpeg/m/716x716/filters:grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['bigair', 'freestyle']
 	},
 	{
 		id: 'matchu',
 		name: 'Matchu Lopes',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Matchu_Lopes.jpg',
+		img: 'https://a.storyblok.com/f/120582/1080x1920/fdc84e2c04/matchu-lopes-portrait.jpg/m/716x716/filters:grayscale(false):blur(0):quality(80):format(webp)',
 		discipline: ['wave']
 	},
 	{
 		id: 'coccoluto',
 		name: 'Gianmaria Coccoluto',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Gianmaria_Coccoluto.jpg',
+		img: 'https://a.storyblok.com/f/120582/3772x4471/a0950b9503/duotone_kiteboarding_gianmariacoccoluto_sicily_profileshot.jpeg/m/716x716/filters:grayscale(false):blur(0):quality(80):format(webp)',
 		discipline: ['freestyle']
 	},
 	{
 		id: 'carew',
 		name: 'James Carew',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/James_Carew.jpg',
+		img: 'https://a.storyblok.com/f/120582/2809x2781/9855b2ff2a/duotone_kiteboarding_jamescarew_tarifa_apparel.jpeg/m/716x716/filters:grayscale(false):blur(0):quality(80):format(webp)',
 		discipline: ['wave', 'bigair']
 	},
 	{
 		id: 'walker',
 		name: 'Lasse Walker',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Lasse_Walker.jpg',
+		img: 'https://a.storyblok.com/f/120582/3333x5000/6bafc2ae7c/lasse_walker_portrait.jpg/m/716x716/filters:focal(1536x2030:1537x2031):grayscale(false):blur(0):quality(80):format(webp)',
 		discipline: ['bigair']
 	},
 	{
 		id: 'hadlow',
 		name: 'Aaron Hadlow',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Aaron_Hadlow.jpg',
-		discipline: ['wakestyle', 'bigair', 'freestyle']
+		img: 'https://a.storyblok.com/f/120582/4000x2667/94c22be61b/aaron_website_profile.jpg/m/716x716/filters:grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['bigair', 'freestyle']
 	},
 	{
 		id: 'romeu',
 		name: 'Reno Romeu',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Reno_Romeo.jpg',
+		img: 'https://a.storyblok.com/f/120582/8142x5431/ef3332b401/duotone_kiteboarding_reno-romeu_apparel.jpg/m/716x716/filters:focal(3422x1862:3423x1863):grayscale(false):blur(0):quality(80):format(webp)',
 		discipline: ['bigair', 'freestyle']
 	},
 	{
 		id: 'vaniersel',
 		name: 'Pippa Van Iersel',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/pippa_van_irsel.jpg',
+		img: 'https://a.storyblok.com/f/120582/1144x1478/1869011903/duotone_kiteboarding_pippavaniersel_firstsession_capetown.jpg/m/716x716/filters:grayscale(false):blur(0):quality(80):format(webp)',
 		discipline: ['bigair', 'freestyle']
-	},
-	{
-		id: 'spiessberger',
-		name: 'Stefan Spiessberger',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Stefan_Spiessberger.jpg',
-		discipline: ['freestyle']
 	},
 	{
 		id: 'matos',
 		name: 'Pedro Matos',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Pedor_Matos_team_page_profile_pic.png',
+		img: 'https://a.storyblok.com/f/120582/8010x5343/7e1a27b56a/duotone_kiteboarding_pedromatos_sansibar_profileshot.jpg/m/716x716/filters:focal(4524x1803:4525x1804):grayscale(false):blur(0):quality(80):format(webp)',
 		discipline: ['wave']
 	},
 	{
 		id: 'font',
 		name: 'Noè Font',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Noe_Font.jpg',
+		img: 'https://a.storyblok.com/f/120582/3428x4696/73a1c01683/duotone_kiteboarding_noefont_sardinia_apparel.jpg/m/716x716/filters:focal(2038x2122:2039x2123):grayscale(false):blur(0):quality(80):format(webp)',
 		discipline: ['wakestyle']
 	},
 	{
 		id: 'novotna',
 		name: 'Paula Novotná',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Paula_Novotna.jpg',
+		img: 'https://a.storyblok.com/f/120582/6336x9504/33d8a13b5c/duotone_kiteboarding_paulanovotna_profileshot.jpeg/m/716x716/filters:focal(0x418:1x419):grayscale(false):blur(0):quality(80):format(webp)',
 		discipline: ['freestyle']
 	},
 	{
 		id: 'arnaus',
 		name: 'Rita Arnaus',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Rita_Arnaus.png',
-		discipline: ['freestyle']
+		img: 'https://a.storyblok.com/f/120582/2869x3310/ac96c38588/duotone_kiteboarding_ritaarnaus_sardinia_apparel.jpeg/m/716x716/filters:grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['freestyle', 'bigair']
 	},
 	{
 		id: 'garat',
 		name: 'Valentin Garat',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Val.JPG',
-		discipline: ['freestyle']
+		img: 'https://a.storyblok.com/f/120582/1200x640/e14f75bc86/duotone_kiteboarding_valentin_garat_profileshot.jpeg/m/716x716/filters:focal(697x228:698x229):grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['freestyle', 'bigair']
 	},
 	{
 		id: 'castro',
 		name: 'Julia Castro',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Julia_Castro.jpg',
-		discipline: ['freestyle']
-	},
-	{
-		id: 'cunningham',
-		name: 'Craig Cunningham',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Craig_Cunningham.jpg',
-		discipline: ['wakestyle', 'freestyle']
+		img: 'https://a.storyblok.com/f/120582/2729x3241/06d9d92ae7/duotone_kiteboarding_juliacastro_lifestyle_profileshot.jpg/m/716x716/filters:grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['freestyle', 'wave']
 	},
 	{
 		id: 'crathern',
 		name: 'Lewis Crathern',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Lewis_Crathern.jpg',
-		discipline: ['bigair']
+		img: 'https://a.storyblok.com/f/120582/2643x2331/f4d5f014dc/duotone_kiteboarding_lewiscrathern_profileshot.jpg/m/716x716/filters:grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['bigair', 'freestyle']
 	},
 	{
 		id: 'tronet',
 		name: 'Jeremie Tronet',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Jeremie_Tronet.jpg',
-		discipline: ['bigair', 'freestyle']
-	},
-	{
-		id: 'carroll',
-		name: 'Colleen Carroll',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Colleen_Carroll.jpg',
-		discipline: ['wakestyle', 'freestyle']
-	},
-	{
-		id: 'cloetens',
-		name: 'Jeremoe Cloetens',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Jerome_Cloetens.jpg',
-		discipline: ['freestyle']
+		img: 'https://a.storyblok.com/f/120582/2207x2377/d21299462b/duotone_kiteboarding_jeremietronet_unionisland_profileshot.jpg/m/716x716/filters:grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['bigair', 'freestyle', 'wave']
 	},
 	{
 		id: 'leon',
 		name: 'Claudia León',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Claudia_Leon_Martinez.jpg',
+		img: 'https://a.storyblok.com/f/120582/2223x3791/47061fc7c8/duotone_kiteboarding_claudialeon_portrait.jpeg/m/716x716/filters:focal(1210x1469:1211x1470):grayscale(false):blur(0):quality(80):format(webp)',
 		discipline: ['freestyle']
 	},
 	{
 		id: 'lambrecht',
 		name: 'Nathalie Lambrecht',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Duotone_Kiteboarding_Nathalie_Lambrecht_rider_profile.jpg',
+		img: 'https://a.storyblok.com/f/120582/5000x3333/f0dcf12851/duotone_kiteboarding_nathalielambrecht_capetown_profileshot.jpg/m/716x716/filters:focal(2944x1344:2945x1345):grayscale(false):blur(0):quality(80):format(webp)',
 		discipline: ['bigair', 'freestyle']
 	},
 	{
 		id: 'hebert',
 		name: 'Tom Hebert',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Tom_Hebert.jpg',
-		discipline: ['bigair']
-	},
-	{
-		id: 'bagnoli',
-		name: 'Francesca Bagnoli',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Francesca_Bagnoli.jpg',
-		discipline: ['freestyle']
+		img: 'https://a.storyblok.com/f/120582/1970x2390/2405c08274/duotone_kiteboarding_tomherbert_newcaledonia_profileshot.jpg/m/716x716/filters:focal(1088x268:1089x269):grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['bigair', 'freestyle']
 	},
 	{
 		id: 'ribeiro',
 		name: 'Sebastian Ribeiro',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Sebastian_Ribero.jpg',
-		discipline: ['wave']
-	},
-	{
-		id: 'court',
-		name: 'Tom Court',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Tom_Court_Streubel_022.jpg',
-		discipline: ['wakestyle', 'freestyle']
-	},
-	{
-		id: 'steindl',
-		name: 'Gabi Steindl',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Gabi_Steindl.jpg',
-		discipline: ['wave']
-	},
-	{
-		id: 'riveras',
-		name: 'Jan Marcos Riveras',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Jan_Marcos_Riveiras.jpg',
-		discipline: ['wave']
-	},
-	{
-		id: 'garashchenko',
-		name: 'Artem Garashchenko',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Artem_Garashenko.jpg',
-		discipline: ['wakestyle', 'freestyle']
-	},
-	{
-		id: 'jungo',
-		name: 'Manuela Jungo',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Manuela_Jungo.jpg',
-		discipline: ['freestyle']
-	},
-	{
-		id: 'mclaughlin',
-		name: 'Patri Mclaughlin',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Patri_McLaughlin.jpg',
-		discipline: ['wave']
+		img: 'https://a.storyblok.com/f/120582/1600x1042/1816af7799/duotone_kiteboarding_sebastianriberio_profileshot.jpeg/m/716x716/filters:focal(1011x277:1012x278):grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['wave', 'freestyle']
 	},
 	{
 		id: 'ulrich',
 		name: 'Edgar Ulrich',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Edgar_Ulrich_Duotone_Kiteboarding_Young_Blood.png',
-		discipline: ['youngblood']
+		img: 'https://a.storyblok.com/f/120582/5000x3333/ddf2efe782/duotone_kiteboarding_edgarulrich_capetown_profileshot.jpg/m/716x716/filters:focal(2187x754:2188x755):grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['bigair', 'freestyle']
 	},
 	{
 		id: 'losserand',
 		name: 'Camille Losserand',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Camille_Losserand_Duotone_Kiteboarding_Young_Blood.png',
-		discipline: ['youngblood']
+		img: 'https://a.storyblok.com/f/120582/7193x4798/5890920957/cami_profile.jpg/m/716x716/filters:focal(4513x1050:4514x1051):grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['wave', 'freestyle']
 	},
 	{
 		id: 'merten',
 		name: 'Keanu Merten',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Keanu_Merten_Duotone_Kiteboarding_Young_Blood.png',
-		discipline: ['youngblood']
+		img: 'https://a.storyblok.com/f/120582/2677x2273/e34f678bf3/duotone_kiteboarding_keanumerten_tarifa_apparel_1.jpg/m/716x716/filters:grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['youngblood', 'wave', 'bigair']
 	},
 	{
 		id: 'gomez',
 		name: 'Beto Gomez',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Betico_Gomez_Duotone_Kiteboarding_Young_Blood.png',
-		discipline: ['youngblood']
+		img: 'https://a.storyblok.com/f/120582/3333x5000/a3a3843f94/duotone_kiteboarding_betogomez_capetown_profileshots.jpg/m/716x716/filters:focal(1662x1060:1663x1061):grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['bigair']
 	},
 	{
 		id: 'tullett',
 		name: 'Max Tullett',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Max_Tullett_Team_pic_Duotone_Kiteboarding_Young_Blood.png',
-		discipline: ['youngblood']
+		img: 'https://a.storyblok.com/f/120582/2443x2768/057d788f3c/duotone_kiteboarding_maxtullet_egypt_porsche.jpg/m/716x716/filters:focal(446x393:447x394):grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['youngblood', 'wave', 'bigair']
 	},
 	{
 		id: 'dabi-ribeiro',
 		name: 'Davi Ribeiro',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Davi_Ribeiro_Da_Silva_Duotone_Kiteboarding_Young_Blood.png',
-		discipline: ['youngblood']
+		img: 'https://a.storyblok.com/f/120582/1707x1280/d3acee9549/duotone_kiteboarding_daviribeiro_tarifa_gkayouth.jpeg/m/716x716/filters:focal(1171x374:1172x375):grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['youngblood', 'freestyle']
 	},
 	{
 		id: 'costa',
 		name: 'Guilherme Costa',
-		img: 'https://www.duotonesports.com/fileadmin/user_upload/Guilhermo_Costa_Duotone_Kiteboarding_Young_Blood.png',
-		discipline: ['youngblood']
+		img: 'https://a.storyblok.com/f/120582/3498x3929/6f8425b68f/duotone_kiteboarding_guilhermecosta_egypt_youngblood_profileshot.jpg/m/716x716/filters:grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['youngblood', 'freestyle']
+	},
+	{
+		id: 'steindl',
+		name: 'Gabi Steindl',
+		img: 'https://a.storyblok.com/f/120582/3641x4608/56e98ab75f/duotone_kiteboarding_gabisteidl_australia_artofadapting_profileshot.jpeg/m/716x716/filters:focal(3570x3610:3571x3611):grayscale(false):blur(0):quality(80):format(webp)',
+		discipline: ['wave']
 	}
 ]
