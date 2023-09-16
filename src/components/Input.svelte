673236f335bfdfac12a9e22408ce4ccb3bfa15da
Carlos Aldaravi <carlosaldaravi11@gmail.com>
2023-09-16 14:12:46 +0200

    change placeholder

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -19,6 +19,6 @@
 		name="rider"
 		id="rider"
 		class="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-900 sm:text-sm sm:leading-6"
-		placeholder="Airton Cozzolino"
+		placeholder="Write the Rider Name.."
 	/>
 </div>
